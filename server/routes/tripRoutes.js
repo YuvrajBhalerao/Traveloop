146d47a39d336e92f77bd944d8db7130e62c1c95
Yuvraj Bhalerao <152587402+YuvrajBhalerao@users.noreply.github.com>
2026-05-10 14:07:07 +0530

    Clean up tripRoutes by removing unused endpoints
    Removed unused route handlers for getting and deleting trips.

--- server/routes/tripRoutes.js
+++ server/routes/tripRoutes.js
@@ -1,16 +1,14 @@
 const express = require("express");
-const router = express.Router();
 
 const {
   createTrip,
   getTrips,
-  getTripById,
-  deleteTrip
 } = require("../controllers/tripController");
 
+const router = express.Router();
+
 router.post("/", createTrip);
+
 router.get("/", getTrips);
-router.get("/:id", getTripById);
-router.delete("/:id", deleteTrip);
 
 module.exports = router;
