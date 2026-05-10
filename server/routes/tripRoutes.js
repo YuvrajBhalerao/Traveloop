75df3a74221a323b24f6ed9666cee970afbc1bf2
Yuvraj Bhalerao <152587402+YuvrajBhalerao@users.noreply.github.com>
2026-05-10 10:46:27 +0530

    Add trip routes for CRUD operations

--- server/routes/tripRoutes.js
+++ server/routes/tripRoutes.js
@@ -1,1 +1,16 @@
+const express = require("express");
+const router = express.Router();
 
+const {
+  createTrip,
+  getTrips,
+  getTripById,
+  deleteTrip
+} = require("../controllers/tripController");
+
+router.post("/", createTrip);
+router.get("/", getTrips);
+router.get("/:id", getTripById);
+router.delete("/:id", deleteTrip);
+
+module.exports = router;
